6db481397df6d8d44af7f478a6390e321eb4c3fd
yogiannanf <yogiannanf@gmail.com>
2024-09-20 10:37:29 +0700

    praktikumjobsheet3

--- Praktikum2/soal1.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  int umur = 18; // Memulai dari umur 18
-
-  for (int i = 0; i < 10; i++) {
-    print('Nama saya adalah Fulan, sekarang berumur $umur');
-    umur--; // Mengurangi umur setiap iterasi
-  }
-}
-
