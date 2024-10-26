5306a57f23f0fc953f45b0d55d11c8d717fc5328
yogiannanf <yogiannanf@gmail.com>
2024-10-26 19:43:48 +0700

    Praktikum 5: Membangun Navigasi di Flutter

--- README.md
+++ README.md
@@ -171,3 +171,19 @@ Atur semua elemen dalam ListView, bukan Column, karena ListView mendukung scroll
 #### *Hasilnya*
 
 <img src = img/hasilprak4.png>
+
+## Praktikum 5: Membangun Navigasi di Flutter
+
+### *- Langkah 1: Siapkan project baru*
+
+### *- Langkah 2: Mendefinisikan Route*
+
+### *- Langkah 3: Lengkapi Kode di main.dart*
+
+### *- Langkah 4: Membuat data model*
+
+### *- Langkah 5: Lengkapi kode di class HomePage*
+
+### *- Langkah 6: Membuat ListView dan itemBuilder*
+
+### *- Langkah 7: Menambahkan aksi pada ListView*
---
+++ belanja/lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
+import 'package:belanja/pages/item_page.dart';
+
+class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return InkWell(
+              child: Card(
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(child: Text(item.name)),
+                      Expanded(
+                        child: Text(
+                          item.price.toString(),
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}     
